c1cdd14a3a82478067836e08f3135ca100078dcd
Bakyt-Bek Murzabaev <winti.ltd@gmail.com>
2025-09-16 21:55:01 +0300

    new update

--- src/main.py
+++ src/main.py
@@ -1,17 +1,10 @@
 import typer
 
-app = typer.Typer()
 
-
-@app.command()
-def create(username: str):
-    print(f"Creating user: {username}")
-
-
-@app.command()
-def delete(username: str):
-    print(f"Deleting user: {username}")
+def main():
+    print("Opening Typer's docs")
+    typer.launch("https://typer.tiangolo.com")
 
 
 if __name__ == "__main__":
-    app()
+    typer.run(main)
